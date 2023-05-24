f767f9cee4fd2a14570012c1ef6f62f7908dd180
npcwalison <gwalisonla@gmail.com>
2023-05-24 16:33:27 -0300

    update

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -2,5 +2,10 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
-
+    background-color: var(--gray-700);
+    width: 100%;
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;

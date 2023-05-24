d66b1024cd679cf863ca4018ee789baedcca4824
npcwalison <gwalisonla@gmail.com>
2023-05-24 16:00:42 -0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+
+function App() {
+  const [] = useState()
+
+  return (
+    <>
+      <GlobalStyle />
+      <p>teste de fonte</p>
+    </>
+  )
+}
+
+export default App
